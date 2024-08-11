46ada0a330e134e304677f90fcbd6dcd354975f2
AviVashishta29 <avivashishta@gmail.com>
2024-08-11 19:28:08 +0530

    done with everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import CustomMouse from "./components/Shared/CustomMouse";
 import DarkModeButton from "./components/Shared/DarkModeButton";
+import useIsMobile from "./hooks/useIsMobile";
 import Home from "./pages/Home";
 import { useThemeStore } from "./store/themeStore";
 
 function App() {
   const { darkMode } = useThemeStore();
-
+  const isMobile = useIsMobile();
   useEffect(() => {
     document.documentElement.setAttribute(
       "data-theme",
@@ -14,12 +17,30 @@ function App() {
     );
   }, [darkMode]);
 
+  useEffect(() => {
+    if (!isMobile) {
+      setTimeout(() => {
+        toast("Just for fun try pressing Ctrl + Enter!", {
+          position: "bottom-right",
+          autoClose: 3000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: darkMode ? "light" : "dark",
+        });
+      }, 3000);
+    }
+  }, [isMobile]);
+
   return (
     // <ReactLenis root>
     <div className="app">
       <Home />
       <DarkModeButton />
       <CustomMouse />
+      <ToastContainer />
     </div>
     // </ReactLenis>
   );
